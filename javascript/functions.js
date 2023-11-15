a16c26010f71f6e4f12f724e60ac869025c3f87a
Steeven506 <sjchaconm@ufpso.edu.co>
2023-11-15 11:14:31 -0500

    practicando js

---
+++ javascript/functions.js
@@ -0,0 +1,20 @@
+console.log("Prueba de conexión")
+const name = "Steeven";
+alert(name)
+console.log(name);
+let array_name = (name);
+console.log(array_name.length);
+let posicion = 0;
+while (posicion<array_name.length){
+    console.log(array_name[posicion]+(posicion+1));
+    posicion++;
+}
+for(let i = 0; i < array_name.length; i++){
+    console.log(array_name[i] + "" + (i + 1));
+}
+
+
+function load_page(){
+    // ejm();
+    // ejm2();
+}
